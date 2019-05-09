3e05430a29302462d5efbed4ab97b76d127df90e
deepak <deepakrao666@gmail.com>
2019-05-09 09:58:49 -0400

    basic project new

---
+++ pro/src/main/java/com/pro/validity2/Validity2Application.java
@@ -0,0 +1,13 @@
+package com.pro.validity2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Validity2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Validity2Application.class, args);
+    }
+
+}
